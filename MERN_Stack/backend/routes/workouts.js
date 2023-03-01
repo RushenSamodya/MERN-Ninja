018a504ffa0b93981c65fddfa848ca86f0cc2b27
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2023-03-01 18:11:16 +0530

    Commmited All

---
+++ MERN_Stack/backend/routes/workouts.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Workout = require('../models/workoutModles');
+
+
+const router = express.Router();
+
+
+// GET all workouts
+
+router.get('/', (req, res)=> {
+    res.json({mssg: 'GET all workouts'})
+});
+
+//GET a single workout
+router.get('/:id', (req, res)=> {
+    res.json({mssg: 'GET a single workout'})
+})
+
+// POST a new workout
+router.post('/', async (req, res)=> {
+    const {title, load, reps} = req.body;
+    
+    try{
+        const Workout = await Workout.create({title, load, reps})
+        res.status(200).json(Workout);
+        
+    } catch (error) {
+        res.status(400).json({error: error.message});
+        
+    }
+
+    
+    res.json({mssg: 'POST a new workout'})
+})
+
+// DELETE a workout
+router.delete('/:id', (req, res)=> {
+    res.json({mssg: 'DELETE a workout'})
+})
+
+// Update a workout
+router.patch('/:id', (req, res)=> {
+    res.json({mssg: 'Update a workout'})
+})
+
+
+module.exports = router;
